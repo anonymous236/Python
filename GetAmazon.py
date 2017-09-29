88c0d6f0f456811240a1e12b0182649cf07b698c
Z.M <mazhouhebut@126.com>
2017-09-29 09:36:16 +0800

    Create GetAmazon.py
    主要是说明怎样伪装请求头部

---
+++ GetAmazon.py
@@ -0,0 +1,13 @@
+def getAmazon() :
+    url = "https://www.amazon.cn/"
+    content = "gp/product/B01M8L5Z3Y"
+
+    #需要设置文件头，将爬虫模拟为浏览器进行对网站的访问
+    kv = {"User-Agent" :"Mozilla/5.0"}
+    try :
+        r = requests.get(url+content, headers=kv)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        print(r.text[1000:2000])
+    except :
+        return "爬取失败"
