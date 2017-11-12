75d4988d0a7a19a96cf8c4db5e7de908e8076463
Z.M <mazhouhebut@126.com>
2017-11-12 15:59:28 +0800

    Update pipelines.py

--- Scrapy/zhihu/zhihu/pipelines.py
+++ Scrapy/zhihu/zhihu/pipelines.py
@@ -20,35 +20,12 @@ class ZhihuPipeline(object):
         Question_id = str(item.get("Question_id"))
         Question_content = item.get("Question_content")
         Content = item.get("Content").replace("\\\"", "\\\\\\\"").replace("\\\\","\\").decode("unicode_escape")
-
+        
+        # 存储为json格式：
         line = "{\"KeyWord\":\""+KeyWord+"\",\"Topic_name\":\""+Topic_name+"\",\"Topic_id\":\""+Topic_id+"\",\"Question_id\":\""+Question_id+"\",\"Question_content\":\""+Question_content+"\",\"Content\":"+Content+"}"
 
-        #os.chdir("/home/hadoop/zhihuSpider/")
-        os.chdir('E:\\')
-        file = codecs.open("zhihu_answer_list.json", mode='ab', encoding='utf-8')
+        os.chdir('路径')
+        file = codecs.open("名称.json", mode='ab', encoding='utf-8')
         file.write(line + "\n")
         file.close()
-
-        '''
-        os.chdir('E:\\')
-        print ("Content: ", item.get("Content"))
-        #print (item.get("Question_content"))
-        file = codecs.open("zhihu_answer_list.json", mode='ab', encoding='utf-8')
-        #print (item)
-        line = json.dumps(dict(item))
-        #print (line)
-        line = line.replace("\\\"", "\\\\\"").replace("\\\\", "\\").replace("\"Content\": \"{","\"Content\": {").replace("\", \"Topic_id\"",", \"Topic_id\"")
-        #print(line.decode("unicode_escape"))
-        #line = line.decode("unicode_escape")
-        file.write(line.decode("unicode_escape") + "\n")
-        file.close()
-        '''
-
-        '''
-        file = codecs.open('1_zhihu.json', mode='ab', encoding='utf-8')
-        line = json.dumps(dict(item)) + '\n'
-        line = line.replace("\"Content\": \"{","\"Content\": {").replace("\", \"Topic_id\"",", \"Topic_id\"")
-        file.write(line.decode("unicode_escape"))
-        file.close()
-        '''
         return item
